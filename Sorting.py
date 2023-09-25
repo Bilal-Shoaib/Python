84e21993b96275591ae7dbcd1fa887153b9c0d73
Bilal <75473298+Bilal-Shoaib@users.noreply.github.com>
2023-09-26 01:20:23 +0500

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,22 @@
+def bubble_sort(array):
+    for _ in range(len(array)):
+        for index in range(len(array)-1):
+            if array[index] > array[index+1]: #For descending '<'
+                temporary = array[index]
+                array[index] = array[index+1]
+                array[index+1] = temporary
+    return array
+
+def insertion_sort(array):
+    for count in range(1, len(array)):
+        current_value, index = array[count], count
+        while index > 0 and array[index-1] > current_value:
+            array[index] = array[index-1]
+            index -= 1
+        array[index] = current_value
+    return array
+
+#Insertion Sort is far more efficient than Bubble Sort
+#This Insertion Sort Algo loop about 26 times for an Array of length 10
+#This Bubble Sort Algo loop about 90 times for an Array of length 10
+#array = ["Ali", "Bilal", "Murtaza", "Rayan", "Babar", "Yasir", "Talal", "Huzaifa", "Soomro", "Azhar"]
