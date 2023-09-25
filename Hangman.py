0546e5d6e1cf36f6b9e3695f6ef326453445b98a
Bilal <75473298+Bilal-Shoaib@users.noreply.github.com>
2023-09-25 23:30:48 +0500

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,57 @@
+from random import choice
+from time import sleep
+clear, up, continue_game = '\x1b[2K', '\033[1A', 'y'
+classes = [{'group':'Automotive', 'array':['ferrari', 'lamborghini', 'pagani', 'koenigsegg', 'bugatti', 'lexus', 'porsche']},
+           {'group':'Watch', 'array':['cartier', 'rolex', 'omega', 'casio', 'tissot', 'fossil', 'diesel', 'guess']},
+           {'group':'Smart Phone', 'array':['samsung', 'google', 'oneplus', 'apple', 'xiaomi', 'oppo', 'motorola']},
+           {'group':'Clothing', 'array':['prada', 'gucci', 'armani', 'dior', 'chanel', 'nike', 'adidas']}]
+###############################################################################################################################################
+def moveupnclearline(num):
+    for _ in range(num): print(up, end = clear)
+###############################################################################################################################################
+def print_hangman(foulsp):
+    if foulsp > 1: print('You lose\n      |`````|')
+    if foulsp > 2: print('    (~_~)   |')
+    if foulsp > 3: print('    /| |\   |')
+    if foulsp > 4: print('    _/ \_   |\n            |\nTTTTTTTTTTTTTTTTT')
+###############################################################################################################################################
+def automate():
+    automatic_selection = choice(classes)
+    print(f'Hint: {automatic_selection["group"]} Brand')
+    return automatic_selection['array']
+###############################################################################################################################################
+def game():
+    print('Guess a letter')
+    main_array = automate()
+    word = choice(main_array)
+    answer, guesses, fouls = '_' * len(word), '', 0
+    while answer != word and fouls < 5:
+        print(answer, end = '\r')
+        character = input()
+        if character in guesses or not (ascii('a') <=  ascii(character.lower()) <= ascii('z')) : 
+            moveupnclearline(1)
+            print('You have already guessed this, or it is an unsupported key. Retry')
+            sleep(2)
+            moveupnclearline(1)
+            continue
+        guesses += character
+        if character in word:    
+            for index in range(len(word)):
+                if character == word[index:index+1]: answer = answer[:index]+answer[index].replace('_',character)+answer[index+1:]
+        else:
+            print(up+'Incorrect :(')
+            sleep(2)
+            fouls += 1
+        moveupnclearline(1)
+    moveupnclearline(2)
+    print(f'YOU WIN!!!!!\nThe correct word was: {word.capitalize()}\nWell Played ;)') if word == answer else print_hangman(fouls)
+    return input('Continue Playing? (Y)es:  ')
+###############################################################################################################################################
+moveupnclearline(1)
+print('Welcome to Hangman :)')
+print('You can only Blunder 5 times\nGOOD LUCK ;)')
+sleep(2)
+moveupnclearline(2)
+while continue_game == 'y':
+    continue_game = game()
+    moveupnclearline(1)
