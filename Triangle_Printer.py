41be5295f19929b10bd38bd3f8f186e33e738db6
Bilal <75473298+Bilal-Shoaib@users.noreply.github.com>
2023-09-26 02:10:44 +0500

    Create Triangle_Printer.py

---
+++ Triangle_Printer.py
@@ -0,0 +1,8 @@
+def triangle_printer() :
+    fill, empty = "/\\", " "
+    rows = input('Number of Rows: ')
+    while not rows.isdigit() or int(rows) < 2:
+        print('Unsupported Input, Retry')
+        rows = input('Number of Rows: ')
+    rows = int(rows)
+    for block in range(rows) : print(empty * (rows - block), fill * (block + 1), sep = "")
