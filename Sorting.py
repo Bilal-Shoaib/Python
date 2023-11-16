4b9f2d319f89ccca64c7418725ea68284b3b093f
Bilal <75473298+Bilal-Shoaib@users.noreply.github.com>
2023-11-16 13:45:56 +0500

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -16,7 +16,24 @@ def insertion_sort(array):
         array[index] = current_value
     return array
 
+def patience_sort(list):
+    piles = []
+    for item in list:
+        new_pile = [item]
+        for pile in piles:
+            if item < pile[-1]:
+                pile.append(item)
+                break
+        else: piles.append(new_pile)
+    sorted_list = []
+    while piles:
+        smallest_pile = min(piles, key=lambda pile: pile[-1])
+        sorted_list.append(smallest_pile.pop())
+        if not smallest_pile: piles.remove(smallest_pile)
+    return sorted_list
+
+
 #Insertion Sort is far more efficient than Bubble Sort
-#This Insertion Sort Algo loop about 26 times for an Array of length 10
-#This Bubble Sort Algo loop about 90 times for an Array of length 10
+#This Insertion Sort Algo loops about 26 times for an Array of length 10
+#This Bubble Sort Algo loops about 90 times for an Array of length 10
 #array = ["Ali", "Bilal", "Murtaza", "Rayan", "Babar", "Yasir", "Talal", "Huzaifa", "Soomro", "Azhar"]
